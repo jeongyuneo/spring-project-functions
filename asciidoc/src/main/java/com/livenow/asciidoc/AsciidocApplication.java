e9e6f51e932736480dae7bf681912ca647ed5be8
jeongyuneo <piq2255@gmail.com>
2021-09-16 18:53:57 +0900

    style: 코드 포맷팅

--- asciidoc/src/main/java/com/livenow/asciidoc/AsciidocApplication.java
+++ asciidoc/src/main/java/com/livenow/asciidoc/AsciidocApplication.java
@@ -9,5 +9,4 @@ public class AsciidocApplication {
     public static void main(String[] args) {
         SpringApplication.run(AsciidocApplication.class, args);
     }
-
 }
--- asciidoc/src/test/java/com/livenow/asciidoc/AsciidocApplicationTests.java
+++ asciidoc/src/test/java/com/livenow/asciidoc/AsciidocApplicationTests.java
@@ -9,5 +9,4 @@ class AsciidocApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
